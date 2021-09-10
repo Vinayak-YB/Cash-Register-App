c144980e4c35d78db91632bfd20b9aebdcf3d6eb
Vinayak Y B <vinayak.bhustali@gmail.com>
2021-09-10 10:36:57 +0530

    Minor changes
    Small changes have been made

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const errorMessage = document.querySelector("#error-message");
-console.log(billAmount.value);
-checkButton.addEventListener("click" , function validateBillAndCashAmount(){
-    if( billAmount.value > 0 ){
+const billAmount = document.getElementById("bill-amount");
+const cashGiven = document.getElementById("cash-given");
+const checkButton = document.getElementById("check-button");
+const errorMessage = document.getElementById("error-message");
 
+checkButton.addEventListener("click" , function validateBillAndCashAmount(){
+    errorMessage.innerText = "";
+    if( billAmount.value > 0 ){
+        console.log(billAmount.value);
     } else {
-        errorMessage.innerText = "Amount insufficient"
+        errorMessage.innerText = "Amount insufficient";
     }
 });
